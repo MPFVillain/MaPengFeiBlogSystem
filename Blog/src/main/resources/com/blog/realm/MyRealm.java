44e34310c40a20d3ee25f6c96eeccda4a0ea9e8c
Coco <652756638@qq.com>
2020-04-24 23:03:34 +0800

    权限验证

--- Blog/src/main/resources/com/blog/realm/MyRealm.java
+++ Blog/src/main/resources/com/blog/realm/MyRealm.java
@@ -1,5 +1,41 @@
 package com.blog.realm;
 
-public class MyRealm {
+ import com.blog.entity.Blogger;
+ import com.blog.service.BloggerService;
+ import javax.annotation.Resource;
+ import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+ import org.apache.shiro.authc.AuthenticationToken;
+ import org.apache.shiro.authc.SimpleAuthenticationInfo;
+ import org.apache.shiro.authz.AuthorizationInfo;
+ import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.Session;
+ import org.apache.shiro.subject.PrincipalCollection;
+ import org.apache.shiro.subject.Subject;
 
+public class MyRealm
+  extends AuthorizingRealm
+{
+ @Resource
+   private BloggerService bloggerService;
+   
+   protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals)
+   {
+     return null;
+   }
+  
+   protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
+     throws AuthenticationException
+   {
+     String userName = (String)token.getPrincipal();
+     Blogger blogger = this.bloggerService.getByUserName(userName);
+     if (blogger != null)
+    {
+       SecurityUtils.getSubject().getSession().setAttribute("currentUser", blogger);
+       AuthenticationInfo authcInfo = new SimpleAuthenticationInfo(blogger.getUserName(), blogger.getPassword(), "xx");
+       return authcInfo;
+     }
+    return null;
+   }
 }
